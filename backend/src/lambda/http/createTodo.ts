a3d6eafac371e84454e2787e83c3d885924c69b9
Chinda_Jordan <jordan.chinda@cm.maviance.com>
2023-03-22 03:48:56 +0100

    Added create todyo Event Handler

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -2,16 +2,36 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
+import { createLogger } from '../../utils/logger'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils';
 import { createTodo } from '../../businessLogic/todos'
 
+const logger = createLogger('AddTodo')
+
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const newTodo: CreateTodoRequest = JSON.parse(event.body)
-    // TODO: Implement creating a new TODO item
 
-    return undefined
+    logger.info('Creating a new TODO')
+    
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    const userId = getUserId(event)
+
+    // TODO: Implement creating a new TODO item
+    const todoItem = await createTodo(userId, newTodo)
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      todoItem
+    })
+  }
+}
+
 )
 
 handler.use(
